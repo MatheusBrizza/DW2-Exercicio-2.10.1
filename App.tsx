430a7e766209f023f66365b8fc2aa0974a271e29
MatheusBrizza <matheusbj95@gmail.com>
2022-07-30 01:14:34 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="main-content">
+      <img src="https://www.elo7.com.br/poster-impresso-netflix-logo/dp/127EDB3" />
+      <div className="loginBody">
+        <div className="loginBox">
+          <h2>Entrar</h2>
+          <form>
+            <div className="inputBox">
+              <input
+                type="email"
+                placeholder="Email ou Número de telefone"
+              ></input>
+            </div>
+            <div className="inputBox">
+              <input type="password" placeholder="Senha"></input>
+            </div>
+
+            <div>
+              <button className="submit">Entrar</button>
+            </div>
+          </form>
+        </div>
+
+        <div className="support">
+          <div className="remember">
+            <span>
+              <input className="checkBox" type="checkbox"></input>
+              <span>Lembre-se de mim</span>
+            </span>
+          </div>
+          <div class="help">
+            <a href="#">Precisa de ajuda?</a>
+          </div>
+        </div>
+        <div class="sign_up">
+          <p>
+            Novo por aqui? <a href="#">Assine agora.</a>
+          </p>
+        </div>
+        <div class="terms">
+          <p>
+            Esta página é protegida pelo Google reCAPTCHA para garantir que você
+            não é um robô. <a href="#">Saiba mais</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-atjpme
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-atjpme)
